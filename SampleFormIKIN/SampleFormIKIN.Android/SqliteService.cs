046e6885fa6637b98c34c36f2dbf4bbb0860d130
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-05-24 16:47:01 +0700

    menambahkan connection

---
+++ SampleFormIKIN/SampleFormIKIN.Android/SqliteService.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using SampleFormIKIN;
+using Xamarin.Forms;
+using SampleFormIKIN.Droid;
+using SQLite.Net;
+using System;
+
+[assembly: Dependency(typeof(SqliteService))]
+namespace SampleFormIKIN.Droid
+{
+    public class SqliteService : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "Pegawai.db3";
+            string documentsPath =
+                   Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            Console.WriteLine(path);
+            if (!File.Exists(path)) File.Create(path);
+            var plat = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
+            var conn = new SQLite.Net.SQLiteConnection(plat, path);
+            return conn;
+
+        }
+    }
+}
